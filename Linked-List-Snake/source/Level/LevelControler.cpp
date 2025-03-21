93d798571a974eac57581a41dc16b206fe1a6e22
Chintan Patel <chintanpatelit024@gmail.com>
2025-03-21 18:05:10 +0530

    Added: Level Model and Controller

--- Linked-List-Snake/source/Level/LevelControler.cpp
+++ Linked-List-Snake/source/Level/LevelControler.cpp
@@ -1,14 +1,32 @@
 #include "Level/LevelController.h"
+#include "Level/LevelModel.h"
+#include "Level/LevelView.h"
 
 namespace Level
 {
-	LevelController::LevelController() {}
+	LevelController::LevelController()
+	{
+		level_model = new LevelModel();
+		level_view = new LevelView();
+	}
 
-	LevelController::~LevelController() {}
+	LevelController::~LevelController()
+	{
+		delete level_model;
+		delete level_view;
+	}
 
-	void LevelController::initialize() {}
+	void LevelController::initialize()
+	{
+		level_view->initialize();
+		level_model->initialize(level_view->getGridWidth(), level_view->getGridHeight());
+	}
 
-	void LevelController::update() {}
+	void LevelController::update() { level_view->update(); }
 
-	void LevelController::render() {}
+	void LevelController::render() { level_view->render(); }
+
+	float LevelController::getCellWidth() const { return level_model->getCellWidth(); }
+
+	float LevelController::getCellHeight() const  { return level_model->getCellHeight(); }
 }
