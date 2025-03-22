55ad393ef8eb2e21ed1debba824e53d1ba47b86e
Chintan Patel <chintanpatelit024@gmail.com>
2025-03-22 11:51:50 +0530

    Added: Draw BodyPart

--- Linked-List-Snake/include/Player/BodyPart.h
+++ Linked-List-Snake/include/Player/BodyPart.h
@@ -11,9 +11,23 @@ namespace Player
 		UI::UIElement::ImageView* bodypart_image;
 
 		sf::Vector2i grid_position;
+		sf::Vector2f screen_position;
 		Direction direction;
 
 		float bodypart_width;
 		float bodypart_height;
-	}
+
+		void createBodyPartImage();
+		void initializeBodyPartImage();
+		void destroy();
+
+	public:
+		BodyPart();
+		~BodyPart();
+
+		void initialize(float width, float height, sf::Vector2i pos, Direction dir);
+		void render();
+
+		sf::Vector2f getBodyPartScreenPosition() const;
+	};
 }
